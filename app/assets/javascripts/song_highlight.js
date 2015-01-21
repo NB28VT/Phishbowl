acb0a9b474bc18945db3a6f72b7a1641c2c0e24e
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-01-21 15:06:39 -0500

    Succesfully integrated js text highlighting for setlist results

--- app/assets/javascripts/song_highlight.js
+++ app/assets/javascripts/song_highlight.js
@@ -1,38 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require foundation
-//= require song_highlight
-//= require highlight_plugin
-//= require_tree .
-
-/*
-
-highlight v5
-
-Highlights arbitrary terms.
-
-<http://johannburkard.de/blog/programming/javascript/highlight-javascript-text-higlighting-jquery-plugin.html>
-
-MIT license.
-
-Johann Burkard
-<http://johannburkard.de>
-<mailto:jb@eaio.com>
-
-*/
-
 jQuery.fn.highlight = function(pat) {
   function innerHighlight(node, pat) {
     var skip = 0;
@@ -71,3 +36,7 @@ jQuery.fn.removeHighlight = function() {
     }
   }).end();
 };
+
+
+console.log(correctSongs);
+$('h2').highlight("setlist");
