9da3727c095d4d68423f92c10679e00571568e28
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-01-20 21:04:21 -0500

    Completed tests for editing predictions

--- spec/features/predictions/random_prediction_spec.rb
+++ spec/features/predictions/random_prediction_spec.rb
@@ -48,26 +48,38 @@ Acceptance criteria
   end
 
 
-  # scenario "A user can edit predictions for a random show" do
-  #   user = FactoryGirl.create(:user)
-  #
-  #   prediction = FactoryGirl.create(:prediction)
-  #
-  #   sign_in_as(user)
-  #
-  #   visit concert_path(prediction.concert)
-  #
-  #   click_on "Edit"
-  #
-  #   select('A Song I Heard the Ocean Sing', from: "Set One Opener")
-  #   select("Kill Devil Falls", from: "Set One Closer")
-  #   select("Golden Age", from: "Set Two Opener")
-  #   select("Harry Hood", from: "Set Two Closer")
-  #   select("Harry Hood", from: "Encore")
-  #   select("Free", from: "Random Pick")
-  #
-  #   click_on "Submit Predictions"
-  #
-  #   expect(page).to have_content ("A Song I Heard the Ocean Sing")
-  # end
+  scenario "A user can edit predictions for a random show" do
+    user = FactoryGirl.create(:user)
+
+    concert = FactoryGirl.create(:concert)
+    FactoryGirl.create(:concert_song, concert_id: concert.id, set_index: 1, play_index: 1)
+    FactoryGirl.create(:concert_song, concert_id: concert.id, set_index: 1, play_index: 2)
+    FactoryGirl.create(:concert_song, concert_id: concert.id, set_index: 1, play_index: 3)
+    FactoryGirl.create(:concert_song, concert_id: concert.id, set_index: 2, play_index: 1)
+    FactoryGirl.create(:concert_song, concert_id: concert.id, set_index: 2, play_index: 2)
+    FactoryGirl.create(:concert_song, concert_id: concert.id, set_index: 2, play_index: 3)
+    FactoryGirl.create(:concert_song, concert_id: concert.id, set_index: 3, play_index: 1)
+
+    song = ConcertSong.first.song
+    other_song = ConcertSong.last.song
+
+    visit new_concert_prediction_path(concert)
+
+    select(song.song_name, from: "Set One Opener")
+    select(song.song_name, from: "Set One Closer")
+    select(song.song_name, from: "Set Two Opener")
+    select(song.song_name, from: "Set Two Closer")
+    select(song.song_name, from: "Encore")
+    select(other_song.song_name, from: "Random Pick")
+
+    click_on "Submit Predictions"
+
+    click_on "Edit"
+
+    select(other_song.song_name, from: "Set One Opener")
+
+    click_on "Submit Predictions"
+
+    expect(page).to have_content (other_song.song_name)
+  end
 end
