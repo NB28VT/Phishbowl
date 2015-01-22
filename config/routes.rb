ed81fdfce49c230bcc6cf2c7dc147eaf28ddf547
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-01-22 15:46:48 -0500

    Fixed delete action for predictions

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :dashboard, only: [:index]
   resources :concerts, only: [:index, :show, :new] do
-    resources :predictions, only: [:new, :create, :show, :edit, :update]
+    resources :predictions, only: [:new, :create, :show, :edit, :update, :destroy]
   end
   resources :about, only: [:index]
 
