24e4fe3629fc6a9339c746f75f13dff27896ff7d
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-01-26 15:54:59 -0500

    commented out database cleaner code

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,23 +1,23 @@
-RSpec.configure do |config|
-
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
-
-end
-
-
-
-# TAKE OUT DB CLEANER?
+# RSpec.configure do |config|
+#
+#   config.before(:suite) do
+#     DatabaseCleaner.clean_with(:truncation)
+#   end
+#
+#   config.before(:each) do
+#     DatabaseCleaner.strategy = :transaction
+#   end
+#
+#   config.before(:each) do
+#     DatabaseCleaner.start
+#   end
+#
+#   config.after(:each) do
+#     DatabaseCleaner.clean
+#   end
+#
+# end
+#
+#
+#
+# # TAKE OUT DB CLEANER?
